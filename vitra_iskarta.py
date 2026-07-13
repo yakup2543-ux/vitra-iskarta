1237b93d81bc3c7a9e848846eef06791d8dd183f
yakup2543-ux <yakup2543@gmail.com>
2026-07-13 14:58:00 +0300

    Update vitra_iskarta.py

--- vitra_iskarta.py
+++ vitra_iskarta.py
@@ -2,12 +2,24 @@ import streamlit as st
 import gspread
 from datetime import datetime
 
-# Dosyayı doğrudan kullanıyoruz
-gc = gspread.service_account(filename="gcp_keys.json")
-sheet = gc.open("VitrA_Iskarta_Tablosu").sheet1
+# Dosya okumuyoruz, doğrudan sözlük yapıyoruz.
+credentials = {
+    "type": "service_account",
+    "project_id": "vitraiskarta",
+    "private_key_id": "22ab8337f6c00e2b3189410fbcf2116a677c445b",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCkF3FbLhXydt3u\n4HRng/vHivrpKHPdGONlzyq5ZiCtzXtVfs2V+guigF2nuhoHcnObOSTPCVWblMN1\nt1O6Ls28qrnnuiwrvbd8QoF0oggrvijnyZ4hKl+rho04P8MvRVi3nE50X2OvZbIB\nbTKyON/X2QLkVVcO+KBdm94XN2v5K5aqGMrXWccOmrmhl7wCrZFbUdSZlsz0uiK/\n87L7rM/Hv7b7VD2PsTnNCKol94yEQ4PSM9sPH0AqzM7I0ogLAIwOjkpP/bS+9WPY\naF5FqTeA3dXnuRo6/0lc+5dN7MMDNW1+b8GRVEFg8tYqmByDPkVJM2nMSMxHLQ7U\nnx4pjKBTAgMBAAECggEACJ7eqq6iIonSIQGTu/h4GsSXZ/ZjF+N91tRqZzERWHAq\nIZS20kE/qTrbMLubMHb30djPsTzJTZIkLqQB9w8MafQCLkVemrGt2Q5ZZLo6eanU\n1lCGSTHDsuwvsRvQckRPY94HVR/JyeYq0t5cwnYwdFOHTl2ZZET1j9VndorH1mA5\nTSKZNXBm7nKwtF/PCQrGDMdwyMuHciqPwmv1Abe9i3BxwW5t+qfOVeJfOuFS/kon\nnfCEM3SDApXnhLc79cSEsGGEfsQZhf8yhUYN9kBjAqyopIiEe/5wtF/zaaWjFqZ0\nh+VQDvfgbqLyfFyp9bWP8xLeeNEgxcm9d5jvDrYaAQKBgQDVz/GMKSisZctFkSEn\np7C/f0Qa8HlOyjHWnlLPfhXbUdNjAWbPFtQX95UezfjQ7tsuk4x2EOjRR7DaYWIk\nsX67UZR92Oohst3EIza2NCFsVj/yseFK0Oe63aycst6LXkPfdSVzL4YKGlCi0epj\nbdFMTdbILSjr9/t2DA6LWSRnAQKBgQDEeAOHi1lNTjOFrm1BPS+Ytv0tQ2A+nce6\nBwfpPXDwzc9j0qeoPRlvCTkrCW+vfh+yQRj/IWc/vVgGWf/XMVIL218A3Qur38xW\n4WRGHy+EJ3GypJ5CU8qfdrWHUBpuWOniB6qF+k9WsHwyW1geLVQp7whZE16DOYTH\nmepFEQI7UwKBgETe5FlXcKiHaYCRDPLvCvnEDrX2u7xrWL5e5SG85WFt6/86Flmi\natMFht0TT5BNQACyuk3ViIjQ5OCS+cAEBGRmFMSsuE3+hXyGMzthc1qoNZUBQyaM\nP/hrKwyWeSS/SnGSFGwT5MMgUtT/dNZuKzq+3+4+za2khTUzEQRFxHoBAoGBAK5+\npSvasQr+/LWrkO/ThxWM2No9sBqNChoIKpeWcVv+f0b6jtvWwGMk/vhhXiewzjgE\np03Z/hjXc3nYr2kSLfvH794VdUtG7vbvIp9BDXPDkLEIkmL4hssQpPO0SnVdVQTi\niqCpsgdDN2NDk3iOXQNwpp/FtSZElIfAhnLeQ/rlvAoGBAKJiNUjDW5glsKpAmItL\nNYadatvPZps1+j6oFWpQJed6yqeKqDK4dPXm9sw2VKev4vuwhH6b84ybb/jzeIEu\na0aQK3SMZLZhyuKSA5ZlUcvnew0IXzLu9nxvTaPtjGMz8lhBqFuKaUnzjRY0i9K/\nW+phSsHhZQMME2V+Rq2RV4Gd\n-----END PRIVATE KEY-----",
+    "client_email": "iskarta@vitraiskarta.iam.gserviceaccount.com",
+    "client_id": "104208590896291725939",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/iskarta%40vitraiskarta.iam.gserviceaccount.com"
+}
+
+# Direkt sözlükten bağlan
+sheet = gspread.service_account_from_dict(credentials).open("VitrA_Iskarta_Tablosu").sheet1
 
 st.title("🏭 VitrA Iskarta Otomatik Kayıt Sistemi")
-
 hat = st.text_input("Hat")
 urun = st.text_input("Ürün İsmi")
 hata = st.text_input("Hata Adı")
