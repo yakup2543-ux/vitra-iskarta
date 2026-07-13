f2f2730c0ab61c8c635decc8e2e1b5e52586efd9
yakup2543-ux <yakup2543@gmail.com>
2026-07-13 14:14:01 +0300

    Update vitra_iskarta.py

--- vitra_iskarta.py
+++ vitra_iskarta.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
-# --- GOOGLE SHEETS AYARLARI ---
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
-client = gspread.authorize(creds)
-sheet = client.open("VitrA_Iskarta_Tablosu").sheet1
+# Google Sheets'e bağlan
+# St.secrets'taki bilgileri doğrudan gspread'in service_account_from_dict yöntemiyle kullanacağız
+sheet = gspread.service_account_from_dict(st.secrets["gcp_service_account"]).open("VitrA_Iskarta_Tablosu").sheet1
 
-# --- ARAYÜZ ---
 st.title("🏭 VitrA Iskarta Otomatik Kayıt Sistemi")
 
 hat = st.text_input("Hat")
@@ -19,10 +15,7 @@ neden = st.text_input("Muhtemel Neden")
 sonuc = st.text_input("Sonuç")
 notlar = st.text_area("Notlar")
 
-if st.button("🚀 Excel'e (Sheets) Kaydet"):
+if st.button("🚀 Excel'e Kaydet"):
     tarih = datetime.now().strftime("%d.%m.%Y %H:%M")
-    
-    # Sheets'e Yaz
     sheet.append_row([tarih, hat, urun, hata, neden, sonuc, notlar])
-    
     st.success("✅ Veri başarıyla Google Sheets'e kaydedildi!")
